49f9639536f62e099fd10669820bf2ad65184637
ZimZeb0y <147079434+ZimZeb0y@users.noreply.github.com>
2024-09-06 21:40:16 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+
+import requests
+import csv
+import pandas as pd
+import os
+
+api_key=os.environ=[api_key]
+dic={}
+players=["Cs_oJay","Folke-","rekz","DEBRE","Rollo-_-",
+         "Nordby","Marden","kjaer666","Hanth","SlyGoingFTW",
+         "Boye-_-","julius777","entire-","MolO11","Bukhavez-",
+         "gleerup","bamme","screens-","caze","L2PaaDetBeat",
+         "Ekke","TroskiDk","_PatchI_","Ep1sk_","BqreBedre"]
+
+for player in players:
+    url = f"https://open.faceit.com/data/v4/players?game=cs2&nickname={player}"
+    headers = {
+        "Accept": "application/json",
+        "Authorization": "Bearer " + api_key
+    }
+    response = requests.get(url, headers=headers)
+    faceitdata = response.json()
+    dic[player]=str(faceitdata["games"]["cs2"]["faceit_elo"])
+
+try:
+    os.remove("Sera Elo.csv")
+except:
+    pass
+
+df = pd.DataFrame(dic,index=[1])
+df.to_csv("Sera Elo.csv")
